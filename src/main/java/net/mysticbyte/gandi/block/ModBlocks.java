b555bec4bdf3c2a205e89e17cc33e5b2db0066a6
Abdullah almousa <abdullahmlwork@gmail.com>
2025-10-21 10:37:11 +0300

    add loot table to the mode + make a block drop item when it mined

--- src/main/java/net/mysticbyte/gandi/block/ModBlocks.java
+++ src/main/java/net/mysticbyte/gandi/block/ModBlocks.java
@@ -19,37 +19,37 @@ public class ModBlocks {
 
     public static final DeferredBlock<Block> UNFORMED_HARMONY_GEAR_BLOCK = registerBlock(
             "unformed_harmony_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(10f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(1f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNFORMED_RADIANCE_GEAR_BLOCK = registerBlock(
             "unformed_radiance_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(10f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(1f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNFORMED_DAWN_GEAR_BLOCK = registerBlock(
             "unformed_dawn_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(10f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(1f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNFORMED_STORM_GEAR_BLOCK = registerBlock(
             "unformed_storm_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(10f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(1f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNIDENTIFIED_GEAR_BLOCK = registerBlock(
             "unidentified_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(2f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> INACTIVE_GEAR_BLOCK = registerBlock(
             "inactive_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(2f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNSTOPPABLE_GEAR_BLOCK = registerBlock(
             "unstoppable_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(50f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_A = registerBlock(
             "corrupted_block_a", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_B = registerBlock(
             "corrupted_block_b", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_C = registerBlock(
             "corrupted_block_c", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_D = registerBlock(
             "corrupted_block_d", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(20f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
