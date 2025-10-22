7845157f5fcab72766b653a73dc858dfce10d709
Abdullah almousa <abdullahmlwork@gmail.com>
2025-10-22 07:38:41 +0300

    add tooltips for ever item and blocks need it for right now

--- src/main/java/net/mysticbyte/gandi/block/ModBlocks.java
+++ src/main/java/net/mysticbyte/gandi/block/ModBlocks.java
@@ -1,7 +1,11 @@
 package net.mysticbyte.gandi.block;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.SoundType;
 import net.minecraft.world.level.block.state.BlockBehaviour;
@@ -12,6 +16,7 @@ import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredBlock;
 import net.neoforged.neoforge.registries.DeferredRegister;
 
+import java.util.List;
 import java.util.function.Supplier;
 
 public class ModBlocks {
@@ -32,31 +37,120 @@ public class ModBlocks {
                     .strength(1f, 20f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNIDENTIFIED_GEAR_BLOCK = registerBlock(
             "unidentified_gear_block", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(2f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(2f, 36000000f).requiresCorrectToolForDrops()
+                    .sound(SoundType.METAL)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unidentified_gear_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unidentified_gear_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredBlock<Block> INACTIVE_GEAR_BLOCK = registerBlock(
             "inactive_gear_block", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(2f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
     public static final DeferredBlock<Block> UNSTOPPABLE_GEAR_BLOCK = registerBlock(
             "unstoppable_gear_block", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_A = registerBlock(
             "corrupted_block_a", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops()
+                    .sound(SoundType.METAL)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_a_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_a_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_B = registerBlock(
             "corrupted_block_b", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops()
+                    .sound(SoundType.METAL)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_b_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_b_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_C = registerBlock(
             "corrupted_block_c", () -> new Block(BlockBehaviour.Properties.of()
-                    .strength(4f, 36000000f).requiresCorrectToolForDrops().sound(SoundType.METAL)));
+                    .strength(4f, 36000000f).requiresCorrectToolForDrops()
+                    .sound(SoundType.METAL)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_c_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_c_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredBlock<Block> CORRUPTED_BLOCK_D = registerBlock(
             "corrupted_block_d", () -> new Block(BlockBehaviour.Properties.of()
                     .strength(4f, 36000000f).requiresCorrectToolForDrops()
-                    .sound(SoundType.METAL)));
+                    .sound(SoundType.METAL)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_d_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.corrupted_d_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredBlock<Block> MUTATOR_BLOCK = registerBlock("mutator_block",
             () -> new MutatorBlock(BlockBehaviour.Properties.of()
                     .strength(4f, 36000000f).requiresCorrectToolForDrops()
-                    .sound(SoundType.ANCIENT_DEBRIS)));
+                    .sound(SoundType.ANCIENT_DEBRIS)){
+                @Override
+                public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.mutator_block_shift"));
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.mutator_block"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
--- src/main/java/net/mysticbyte/gandi/item/ModItems.java
+++ src/main/java/net/mysticbyte/gandi/item/ModItems.java
@@ -1,11 +1,17 @@
 package net.mysticbyte.gandi.item;
 
+import net.minecraft.client.gui.screens.Screen;
+import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.TooltipFlag;
 import net.mysticbyte.gandi.GandI;
 import net.mysticbyte.gandi.item.custom.*;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.neoforge.registries.DeferredItem;
 import net.neoforged.neoforge.registries.DeferredRegister;
+
+import java.util.List;
 
 public class ModItems {
 
@@ -16,11 +22,43 @@ public class ModItems {
     public static final DeferredItem<Item> INACTIVE_GEAR = ITEMS.register("inactive_gear",
             () -> new Item(new Item.Properties()) );
     public static final DeferredItem<Item> UNIDENTIFIED_GEAR = ITEMS.register("unidentified_gear",
-            () -> new Item(new Item.Properties()) );
+            () -> new Item(new Item.Properties()){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unidentified_gear_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unidentified_gear"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            } );
     public static final DeferredItem<Item> UNFORMED_STORM_GEAR = ITEMS.register("unformed_storm_gear",
             () -> new Item(new Item.Properties()) );
     public static final DeferredItem<Item> UNFORMED_DAWN_GEAR = ITEMS.register("unformed_dawn_gear",
-            () -> new Item(new Item.Properties()) );
+            () -> new Item(new Item.Properties()){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unformed_dawn_gear_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.unformed_dawn_gear"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            } );
     public static final DeferredItem<Item> UNFORMED_RADIANCE_GEAR = ITEMS.register("unformed_radiance_gear",
             () -> new Item(new Item.Properties()) );
     public static final DeferredItem<Item> UNFORMED_HARMONY_GEAR = ITEMS.register("unformed_harmony_gear",
@@ -35,16 +73,97 @@ public class ModItems {
             () -> new Item(new Item.Properties()) );
 
     public static final DeferredItem<Item> DAWN_WAND = ITEMS.register("dawn_wand",
-            () -> new DawnWand(new Item.Properties().durability(2)));
+            () -> new DawnWand(new Item.Properties().durability(2)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.dawn_wand_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.dawn_wand"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> STORM_WAND = ITEMS.register("storm_wand",
-            () -> new StormWand(new Item.Properties().durability(2)));
+            () -> new StormWand(new Item.Properties().durability(2)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.storm_wand_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.storm_wand"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> HARMONY_WAND = ITEMS.register("harmony_wand",
-            () -> new HarmonyWand(new Item.Properties().durability(2)));
+            () -> new HarmonyWand(new Item.Properties().durability(2)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.harmony_wand_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.harmony_wand"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
     public static final DeferredItem<Item> RADIANCE_WAND = ITEMS.register("radiance_wand",
-            () -> new RadianceWand(new Item.Properties().durability(2)));
+            () -> new RadianceWand(new Item.Properties().durability(2)){
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.radiance_wand_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.radiance_wand"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static final DeferredItem<Item> BREAD_UNSTOPPABLE = ITEMS.register("bread_unstoppable",
-            () -> new Item(new Item.Properties().food(ModFoodProperties.BREAD_UNSTOPPABLE)));
+            () -> new Item(new Item.Properties().food(ModFoodProperties.BREAD_UNSTOPPABLE))
+            {
+                @Override
+                public void appendHoverText(ItemStack stack, TooltipContext context, List<Component> tooltipComponents, TooltipFlag tooltipFlag) {
+
+                    if (Screen.hasShiftDown()){
+
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.bread_unstoppable_shift"));
+
+                    }else {
+                        tooltipComponents.add(Component.translatable(
+                                "tooltip.grindingandindustrialization.bread_unstoppable"));
+                    }
+
+                    super.appendHoverText(stack, context, tooltipComponents, tooltipFlag);
+                }
+            });
 
     public static void register(IEventBus eventBus){
 
